667f7f2f8f2090b840c0a90ddb17e2650c5ac8a7
Monisha Challa <monishareddy0912@gmail.com>
2025-06-27 16:07:24 +0530

    START new

--- android/app/src/main/kotlin/com/example/moodairy/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moodairy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/activity.dart
+++
@@ -1,7 +0,0 @@
-class Activity{
-
-  String image;
-  String name;
-  bool selected;
-  Activity(this.image,this.name,this.selected);
-}
